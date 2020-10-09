18901c6c8cb0e24f453dac0b362874864c6a2595
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2020-10-09 23:22:27 +0900

    非同期処理まとめ Fixes #3
    🚧WIP:FileSystemWatcherをつかってアレコレ

---
+++ Draft/FileSystemWatcherサンプル.Tests.ps1
@@ -0,0 +1,33 @@
+﻿using namespace System.IO
+
+Describe "イベントを購読" {
+    Context "Changed" {
+        BeforeAll {
+            $watcher = [FileSystemWatcher]::new($PWD)
+        }
+        It "直接処理する" -Skip {
+            $action = {
+                Get-Variable -Scope 0 | Format-List | Out-Host
+            }
+            Register-ObjectEvent $watcher -EventName Changed -SourceIdentifier changedEvent -Action $action
+        }
+        It "新しくイベントを発行する" -Skip {
+            $action = {
+                New-Event -SourceID "PowerShell.FileChanged" -Sender $sender -EventArguments $event > $null
+            }
+            Register-ObjectEvent $watcher -EventName Changed -SourceIdentifier changedEvent -Action $action
+        }
+        It "Thread Jobを発行する" -Skip {
+            $action = {
+                # Get-Variable -Scope 0  | Start-ThreadJob { "event fired."; $input } -Name "watcherJob$(New-Guid)"
+            }
+            Register-ObjectEvent $watcher -EventName Changed -SourceIdentifier changedEvent -Action $action
+        }
+
+        AfterEach {
+            Unregister-Event changedEvent
+        }
+    }
+}
+
+
