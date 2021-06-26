0a769df860d40cd1ca938ddf2195ab570eaceb65
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-06-26 15:59:48 +0900

    WIP：Binaryモジュール検証

---
+++ Module/Pinvoke/BinaryModule/src/PsPInveke/ResolveMyCmdletCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.Windows.Sdk;
+using static Microsoft.Windows.Sdk.PInvoke;
+
+namespace PsPInveke
+{
+     class Test
+    {
+        static Type Native => Type.GetType("PInvoke");
+
+        public static BOOL OutputWindow(HWND hwnd, LPARAM lParam)
+        {
+            Console.WriteLine(hwnd.Value);
+            return true;
+        }
+
+        public static BOOL Do()
+        {
+            return EnumWindows(OutputWindow, (LPARAM)0);
+        }
+
+
+    }
+
+    [Cmdlet(VerbsDiagnostic.Resolve, "MyCmdlet")]
+    public class ResolveMyCmdletCommand : PSCmdlet
+    {
+        [Parameter(Position = 0)]
+        public Object InputObject { get; set; }
+
+        protected override void EndProcessing()
+        {
+            var t = typeof(Test);
+            this.WriteObject(t);
+            base.EndProcessing();
+        }
+    }
+}
